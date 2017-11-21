78a4ce5fb17ec53c1204dff51afb8e2321ae045f
lakshay <lakshaygaba67@gmail.com>
2017-11-21 23:46:17 +0530

    final commit

--- main.js
+++ main.js
@@ -14,6 +14,53 @@ function toggleSong() {
     }
 }
 
+$(document).ajaxStart(function(){
+    $("#wait").css("display", "block");
+});
+$(document).ajaxComplete(function(){
+    $("#wait").css("display", "none");
+});
+$("button").click(function(){
+    $("#txt").load("demo_ajax_load.asp");
+});
+
+$('#replace-song').on('click', function (){
+    var songName = $('#song-name-input');
+    var songArtist = $('#song-artist-input');
+    var songAlbum = $('#song-album-input');
+    var songDuration = $('#song-duration-input');
+    var songURL = $('#song-url-input');
+    var songVideo = $('#song-video-input');
+    var songLyrics = $('#song-lyrics-input');
+    var songImage = $('#song-image-input');
+
+    songs[3] = {
+        'name': songName.val(),
+        'artist': songArtist.val(),
+        'album': songAlbum.val(),
+        'duration': songDuration.val(),
+        'fileName': songURL.val(),
+        'videoLink': songVideo.val(),
+        'lyricsLink': songLyrics.val(),
+        'image': songImage.val()
+    };
+
+    songName.val('');
+    songArtist.val('');
+    songAlbum.val('');
+    songDuration.val('');
+    songURL.val('');
+    songVideo.val('');
+    songLyrics.val('');
+    songImage.val('');
+    setupApp();
+
+});
+
+$("#song1").hover(function(){
+    $('.song-lyrics').removeClass('hidden');
+    });
+
 $('.welcome-screen button').on('click', function() {
     var name = $('#name-input').val();
 
@@ -27,17 +74,21 @@ $('.welcome-screen button').on('click', function() {
     }
     else {
         $('#name-input').addClass('error');
-    }
-});
-$('.play-icon').on('click',function() {
-    toggleSong()
-});
-$('body').on('keypress',function(event) {
-    if (event.keyCode == 32){
-        toggleSong()
+        $('#error-text').removeClass('hidden');
     }
 });
 
+$('#logout').click(function(){
+    $('.welcome-screen').removeClass('hidden');
+    $('.main').addClass('hidden');
+    $('#name-input').val("");
+    $('#name-input').removeClass('error');
+    $("#error-text").addClass('hidden');
+});
+
+$('.play-icon').on('click',function() {
+    toggleSong()
+});
 function fancyTimeFormat(time)
 {
     // Hours, minutes and seconds
@@ -59,15 +110,16 @@ function fancyTimeFormat(time)
 
 function updateCurrentTime() {
     var song = document.querySelector('audio');
-    // console.log(song.currentTime);
-    // console.log(song.duration);
     var currentTime = Math.floor(song.currentTime);
-    currentTime = fancyTimeFormat(currentTime);
-
     var duration = Math.floor(song.duration);
-    duration = fancyTimeFormat(duration)
+    var remainingTime = duration - currentTime;
+    currentTime = fancyTimeFormat(currentTime);
+    duration = fancyTimeFormat(duration);
+    remainingTime = fancyTimeFormat(remainingTime);
+    console.log(currentTime);
     $('.time-elapsed').text(currentTime);
     $('.song-duration').text(duration);
+    $('.time-left').text(remainingTime);
 }
 var fileNames = ['song1.mp3','song2.mp3','song3.mp3','song4.mp3'];
 
@@ -100,26 +152,34 @@ function addSongNameClickEvent(songObj,position) {
 
 var songs = [];
 
-
+$('.total-songs').text("Songs: " + songs.length);
 
 function fetchSongs() {
 
     $.ajax({
         'url': 'https://jsonbin.io/b/59f713154ef213575c9f652f',
         'dataType': 'json',
+
         'method': 'GET',
         'success': function (responseData) {
             // do something with the data here
             songs = responseData ;
             setupApp();
+            $('.total-songs').text("Songs: " + songs.length);
+
         }
     }) ;
+    $('body').on('keypress',function(event) {
+        if (event.keyCode == 32 || event.keyCode == 80 || event.keyCode == 112){
+            toggleSong()
+        }
+    });
 
 }
 
 function setupApp() {
     changeCurrentSongDetails(songs[0]);
-    for(var i =0; i < songs.length;i++) {
+    for(var i =0; i < songs.length ;i++) {
         var obj = songs[i];
         var name = '#song' + (i+1);
         var song = $(name);
@@ -127,6 +187,8 @@ function setupApp() {
         song.find('.song-artist').text(obj.artist);
         song.find('.song-album').text(obj.album);
         song.find('.song-length').text(obj.duration);
+        song.find(' .song-lyrics').html('<a target="_blank" href="' + obj.lyricsLink +'">See Lyrics</a>');
+        song.find(' .song-video').html('<a target="_blank" href="' + obj.videoLink +'">Watch Video</a>');
         addSongNameClickEvent(obj,i+1);
     }
 
